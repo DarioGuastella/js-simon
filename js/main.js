d9c913951a3a0a84087fbe81c29a3f6b83d7fdac
DarioGuastella <jimmyguastella@gmail.com>
2023-11-09 18:19:00 +0100

    feat: chiede all'utente 5 numeri,li salva in array

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const pageDiv = document.getElementById("container");
 //Genero 5 numeri in maniera randomica, all'interno di un array.
 
 let RandomNumbers = [];
+let selectedNumbers = [];
 
 while (RandomNumbers.length < 5) {
     let number = numeroRandom(1, 99);
@@ -16,9 +17,22 @@ while (RandomNumbers.length < 5) {
 }
 console.log(`Numeri randomici: ${RandomNumbers}`);
 
+//Aspetto 30 secondi ed eseguo funzione
+const myTimer =setTimeout(Timer, 3*1000); //ToDo: cambia in 30 secondi
+
 
 ////////////////////// FUNZIONI ////////////////////////////////
 
 function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+///
+
+function Timer() {
+    container.innerHTML = "";
+    while (selectedNumbers.length < 5) {
+        let number = prompt("Inserisci uno dei numeri visualizzati");
+        selectedNumbers.push(number);
+    }
+  }
