c15cac271b45e3434048db789433d1df040fb1b3
DarioGuastella <jimmyguastella@gmail.com>
2023-11-09 17:59:57 +0100

    style:crea contenuto; feat:stampa numeri in pagina

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const pageDiv = document.getElementById("container");
 //Genero 5 numeri in maniera randomica, all'interno di un array.
 
 let RandomNumbers = [];
@@ -5,7 +6,11 @@ let RandomNumbers = [];
 while (RandomNumbers.length < 5) {
     let number = numeroRandom(1, 100);
     if (RandomNumbers.indexOf(number)=== -1) {
+        const paragraph = document.createElement("p");
         RandomNumbers.push(number);
+        paragraph.innerText += number;
+        pageDiv.appendChild(paragraph);
+        paragraph.classList.add("numbers");
     }
 
 }
